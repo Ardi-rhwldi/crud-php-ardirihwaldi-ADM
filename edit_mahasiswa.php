62dbea065a6b2c6e79f1bc6fc1dc85de97b47fa2
Ardi-rhwldi <ardirihwaldi12@gmail.com>
2024-11-24 16:07:21 +0700

    Add files via upload

--- edit_mahasiswa.php
+++ edit_mahasiswa.php
@@ -11,16 +11,31 @@ $data = $result -> fetch_assoc();
 <head>
     <meta charset="UTF-8">
     <title>Edit Data Mahasiswa</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <h2>Edit Data Mahasiswa</h2>
     <FORM action="proses_edit.php" method="POST">
-        <input type="hidden" name="id" value="<?php echo $data['id']; ?>">
-        Nama: <input type="text" name="nama" value="<?php echo $data['nama']; ?>"><br>
-        NIM: <input type="text" name="nim" value="<?php echo $data['nim']; ?>"><br>
-        Email: <input type="text" name="email" value="<?php echo $data['email']; ?>"><br>
-        Nomor: <input type="text" name="nomor" value="<?php echo $data['nomor']; ?>"><br>
-        Jurusan: <input type="text" name="jurusan" value="<?php echo $data['jurusan']; ?>"><br>
+        <table border="1">
+            <tr>
+                <td>
+                    <input type="hidden" name="id" value="<?php echo $data['id']; ?>">
+                    Nama: <input type="text" name="nama" value="<?php echo $data['nama']; ?>"><br>
+                </td>
+                <td>
+                    NIM: <input type="text" name="nim" value="<?php echo $data['nim']; ?>"><br>
+                </td>
+                <td>
+                    Email: <input type="text" name="email" value="<?php echo $data['email']; ?>"><br>
+                </td>
+                <td>
+                    Nomor: <input type="text" name="nomor" value="<?php echo $data['nomor']; ?>"><br>
+                </td>
+                <td>   
+                    Jurusan: <input type="text" name="jurusan" value="<?php echo $data['jurusan']; ?>"><br>
+                </td>
+            </tr>
+        </table>
         <input type="submit" value="Simpan">
     </FORM>
 </body>
--- tambah_mahasiswa.php
+++ tambah_mahasiswa.php
@@ -4,21 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <title>Tambah Data Mahasiswa</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <h2>Tambah Data Mahasiswa</h2>
     <form action="proses_tambah.php" method="POST">
-        <label>Nama :</label>
-        <input type="text" name="nama" required><br>
-        <label>NIM :</label>
-        <input type="text" name="nim" required><br>
-        <label>Email :</label>
-        <input type="email" name="email" required><br>
-        <label>Nomor :</label>
-        <input type="text" name="nomor" required><br>
-        <label>Jurusan</label>
-        <input type="text" name="jurusan" required><br>
-
+        <table border="1">
+            <tr>
+                <td>
+                    <label>Nama :</label>
+                    <input type="text" name="nama" required><br>
+                </td>
+                <td>
+                    <label>NIM :</label>
+                    <input type="text" name="nim" required><br>
+                </td>
+                <td>
+                    <label>Email :</label>
+                    <input type="email" name="email" required><br>
+                </td>
+                <td>
+                    <label>Nomor :</label>
+                    <input type="text" name="nomor" required><br>
+                </td>
+                <td>
+                    <label>Jurusan</label>
+                    <input type="text" name="jurusan" required><br>
+                </td>
+            </tr>
+        </table>
         <input type="submit" value="Tambah">
     </form>
 </body>
